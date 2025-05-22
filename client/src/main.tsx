9afadb51fce5635e93bb4bbee84d4e1328cf4b81
tomasz-wlodarczyk-pawatech <tomasz.wlodarczyk@pawatech.com>
2025-05-22 11:46:55 +0200

    initaa

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Only import used font weights
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
+
 
 createRoot(document.getElementById("root")!).render(<App />);
